a791436923a8da0dccefecc98ea66605535d68da
melody <melody@unplug.red>
2019-04-25 09:58:57 +0300

    google ad redirect

---
+++ htdocs/ohhi.php
@@ -0,0 +1,26 @@
+<html>
+</html>
+<?php
+$color = "#000000";
+$title = "ohhi";
+include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
+		<script>
+			gtag('event', 'conversion', {'send_to': 'AW-971029553/6cGDCLSmv5oBELH4gs8D'});
+		</script>
+        <script type="text/javascript">
+            window.location.href = "/"
+        </script>
+        <style>
+			body {
+				text-align: center;
+				text-anchor: middle;
+				margin: calc(50vh - 14px) 0;
+				position: absolute;
+				width: 100vw;
+			}
+		</style>
+    </head>
+    <body>
+        Page moved to <a href='/'>here</a>.
+    </body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
