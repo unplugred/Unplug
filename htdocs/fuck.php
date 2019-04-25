cf3ef9540d7cf5bba46f42528a6393838fd652d2
red <arihanan@protonmail.com>
2019-04-25 09:59:43 +0300

    nasty page

---
+++ htdocs/fuck.php
@@ -0,0 +1,18 @@
+<?php
+$color = "#000000";
+$title = "fuck";
+include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
+		<style>
+			.mainimage {
+				margin-top: calc(50vh - 232px);
+				margin-left: calc(50vw - 200px);
+				display: block;
+				width: 400px;
+				height: 487px;
+				background-image: url("/assets/fuck.gif");
+			}
+		</style>
+	</head>
+	<body>
+		<div class="mainimage"></div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
