f6c86b3d36e7ecb6e37e502fde651ce1e84f38cc
red <arihanan@protonmail.com>
2019-05-01 07:33:45 +0300

    added link to hyperaction

--- htdocs/important.php
+++ htdocs/important.php
@@ -3,7 +3,7 @@ $color = "#000000";
 $title = "important";
 include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 		<style>
-			body{
+			body {
 				background-image: url("/assets/important.png");
 				background-repeat: no-repeat;
 				background-position: bottom right;
@@ -14,10 +14,20 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 				margin: calc(50vh - 14px) auto;
 				display: table;
 			}
+
+			#corner {
+				display: block;
+				position: absolute;
+				width: 250px;
+				height: 250px;
+				bottom: 0;
+				right: 0;
+			}
 		</style>
 	</head>
 	<body>
 		<div class="mainimage">
 			<a href="/assets/important.txt" download="important.txt">important.txt</a>
+			<a href="/hyperaction" id="corner"></a>
 		</div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
