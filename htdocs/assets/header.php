018f18901517f8d048f433c597731f465a0379b3
melody <melody@unplug.red>
2019-05-03 11:10:07 +0300

    changed tracking from google to matomo

--- htdocs/assets/header.php
+++ htdocs/assets/header.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52381925-3"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
+<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/tracking.php'; ?>
 
-			gtag('config', 'UA-52381925-3');
-		</script>
-		
 		<meta name="theme-color" content="<?php echo $color ?>">
 		<link rel="icon" type="image/png" href="/assets/shortcut-icon.png">
 		<link rel="shortcut icon" href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/assets/shortcut-icon.png">
@@ -33,5 +26,5 @@
 		<meta name="description" content="unplug your mind for best expirience.">
 		<meta property="og:image" content="https://<?php echo $_SERVER['HTTP_HOST'] ?>/assets/h.gif">
 		<meta name="twitter:image" content="https://<?php echo $_SERVER['HTTP_HOST'] ?>/assets/h.gif">
-		
+
 		<link rel=StyleSheet href="/assets/unplug.css" type="text/css" media=screen>
