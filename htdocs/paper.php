afb862fd3b3f9053746682be17a46716b61b8ebc
red <arihanan@protonmail.com>
2019-05-05 21:09:11 +0300

    paper cherry pages

---
+++ htdocs/paper.php
@@ -0,0 +1,15 @@
+<?php
+$color = "#000000";
+$title = "paper";
+include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
+		<style>
+			body{
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		</style>
+	</head>
+	<body>
+		<a href="/cherry"><img src="/assets/paper.png"/></a>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
