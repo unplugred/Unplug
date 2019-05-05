e26da82c09c2150a9c7f89b67b5395c7bf506d0e
red <arihanan@protonmail.com>
2019-05-05 07:24:18 +0300

    time

---
+++ htdocs/time.php
@@ -0,0 +1,16 @@
+<?php
+$color = "#000000";
+$title = "important";
+include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
+		<style>
+			body {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				flex-direction: column;
+			}
+		</style>
+	</head>
+	<body>
+			<img src="http://hitwebcounter.com/counter/counter.php?page=7068559&style=0015&nbdigits=5&type=page&initCount=0"/><br/><img src="/assets/time.gif"/>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
