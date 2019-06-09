b0b89afedc8773cd6a860a74b439fc98a93000de
melody <melody@unplug.red>
2019-06-09 22:13:36 +0300

    wildcard subdomain cause why not

---
+++ htdocs/_wildcard_/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Welcome</title>
+
+		<meta name="theme-color" content="#FF0000">
+		<link rel="icon" type="image/png" href="/shortcut-icon.png">
+		<link rel="shortcut icon" href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/shortcut-icon.png">
+		<link rel="apple-touch-icon-precomposed" href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/apple-touch-icon.png">
+
+		<meta name="keywords" content="ari,hanan,arihanan,red,redflux,red#3510,unplug,Welcome">
+		<meta name="dcterms.rightsHolder" content="Ari Hanan">
+
+		<meta property="og:site_name" content="red">
+		<meta name="twitter:site" content="red">
+		<meta property="og:url" content="https://<?php echo $_SERVER['HTTP_HOST'] ?>">
+		<meta name="twitter:url" content="https://<?php echo $_SERVER['HTTP_HOST'] ?>">
+		<meta property="og:type" content="article">
+		<meta name="twitter:card" content="summary">
+		<meta property="og:title" content="Welcome">
+		<meta name="twitter:title" content="Welcome">
+		<meta property="og:description" content="unplug your mind for best experience.">
+		<meta name="twitter:description" content="unplug your mind for best experience.">
+		<meta name="description" content="unplug your mind for best expirience.">
+		<meta property="og:image" content="https://<?php echo $_SERVER['HTTP_HOST'] ?>/h.gif">
+		<meta name="twitter:image" content="https://<?php echo $_SERVER['HTTP_HOST'] ?>/h.gif">
+
+		<style>
+			body{
+				animation: anim 50s linear infinite;
+				width: 100vw;
+				height: 100vh;
+				overflow-y: hidden;
+				overflow-x: hidden;
+			}
+
+			@keyframes anim{
+				0%{
+					background-color: #FF0000;
+				}
+				17%{
+					background-color: #FFFF00;
+				}
+				33%{
+					background-color: #00FF00;
+				}
+				50%{
+					background-color: #00FFFF;
+				}
+				67%{
+					background-color: #0000FF;
+				}
+				83%{
+					background-color: #FF00FF;
+				}
+				100%{
+					background-color: #FF0000;
+				}
+			}</style></head><body></body></html>
