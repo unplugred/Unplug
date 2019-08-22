d022e1c80d13e1da35f27d303a809a824ca7b5ec
melody <melody@unplug.red>
2019-08-22 11:59:22 +0300

    ppl

--- htdocs/fuck.php
+++ htdocs/fuck.php
@@ -1,5 +1,4 @@
 <?php
-$color = "#000000";
 $title = "fuck";
 include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 		<style>
@@ -11,8 +10,17 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 				height: 487px;
 				background-image: url("/assets/fuck.gif");
 			}
+
+			#linktonextpage {
+				top: 196px;
+				left: 196px;
+				width: 116px;
+				height: 74px;
+				display: block;
+				position: relative;
+			}
 		</style>
 	</head>
 	<body>
-		<div class="mainimage" title="fuck you."></div>
+		<div class="mainimage"><a title="fuck you." id="linktonextpage" href="/ppl"></a></div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
