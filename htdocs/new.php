bf5c0adc725b06cb44d97d71c6b6c6b5d541491d
melody <melody@unplug.red>
2019-08-23 05:36:01 +0300

    added 'new' page

---
+++ htdocs/new.php
@@ -0,0 +1,24 @@
+<?php
+$color = "#FFFFFF";
+$title = "new";
+include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';
+?>
+		<style>
+			body {
+				background-color: white;
+			}
+
+			#link{
+				background-image: url("/assets/new.png");
+				position: absolute;
+				top: 2vw;
+				left: 2vw;
+				width: 296px;
+				height: 223px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="link" title="a new plane of existence,
+reserved just for you."></div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
