6dae49c71d5d3fb7e5f1db47eb9fb025924f08c7
melody <melody@unplug.red>
2019-09-14 22:49:08 +0300

    bye now using canvas

--- htdocs/bye.php
+++ htdocs/bye.php
@@ -2,32 +2,6 @@
 $title = "bye";
 include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 		<style>
-			.mainimage {
-				width: 306px;
-				height: 325px;
-				background-size: contain;
-				background-image: url("/assets/bye/fakery.png");
-				background-repeat: no-repeat;
-				background-position: center;
-				margin-top: calc(50vh - 162px);
-				margin-left: calc(50vw - 153px);
-			}
-
-			.bleh{
-				width: 82px;
-				height: 36px;
-				position: absolute;
-				background-image: url("/assets/bye/bye.png");
-			}
-
-			.close{
-				display: block;
-				width: 100vw;
-				height: 100vh;
-				z-index:1000;
-				position: absolute;
-			}
-
 			#fade{
 				position: absolute;
 				width: 100vw;
@@ -40,43 +14,46 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 			}
 
 			@keyframes fade {
-				0% {
-					background-color: black;
-					height: 100vh;
-				}
-				25% {
-					background-color: #000000bd;
-				}
-				50% {
-					background-color: #0000007f;
-				}
-				75% {
-					background-color: #0000003f;
-				}
-				100% {
-					background-color: transparent;
-				}
+				0% { background-color: black; height: 100vh; }
+				25% { background-color: #000000bd; }
+				50% { background-color: #0000007f; }
+				75% { background-color: #0000003f; }
+				100% { background-color: transparent; }
 			}
 
-			#black{
-				background-color: black;
+			#canva {
 				position: absolute;
-				width: 100vw;
-				height: 100vh;
-				z-index: -1;
+				top: 0;
+				left: 0;
 			}
 		</style>
 	</head>
 	<body>
-		<div id="black"></div>
-		<div class="mainimage" id="appendme" title="bye"></div>
-		<div id="fade"></div>
+		<img id="bg" title="bye" src="/assets/bye/fakery.png" style="display: none;"></div>
+		<img id="bye" title="bye" src="/assets/bye/bye.png" style="display: none;"></div>
+		<canvas id="canva"></canvas>
+		<div id="fade" style="display: none;"></div>
 		<script>
-			for (x = 0; x < Math.min(window.innerWidth/2 + 125, window.innerHeight/2 + 121); x += 40) {
-				var div = document.createElement("div");
-				div.style.margin = (42 + x) + "px 0 0 " + (28 + x) + "px";
-				div.className = "bleh";
-				document.getElementById("appendme").appendChild(div);
+			var canvas = document.getElementById("canva");
+			var ctx = canvas.getContext("2d");
+			var bg = document.getElementById("bg");
+			var bye = document.getElementById("bye");
+
+			window.onload = function(){
+				document.getElementById("fade").style.display = null;
+				redraw();
+			};
+			window.onresize = function(){ redraw(); };
+
+			function redraw()
+			{
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+				var hw = Math.round(canvas.width*.5);
+				var hh = Math.round(canvas.height*.5);
+				ctx.drawImage(bg, hw - 153, hh - 163);
+				for (x = -125; x < Math.min(hw, hh + 3); x += 40)
+					ctx.drawImage(bye, x + hw, x + hh + 3);
 			}
 		</script>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
