35b49693f9bf2b066410b975014d3d15970a5a55
red <arihanan@protonmail.com>
2019-09-16 17:22:49 +0300

    live teapot + performance optimizations

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,7 +9,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 				position: fixed;
 			}
 
-			.clock {
+			#clock {
 				width: 106px;
 				height: 125px;
 				background-image: url("/assets/unplug/clock.png");
@@ -26,7 +26,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 				opacity: .5;
 			}
 
-			.grid {
+			#grid {
 				width: 76px;
 				height: 225px;
 				background-image: url("/assets/unplug/grid.png");
@@ -34,12 +34,17 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 				margin-left: calc(63vw + 210px);
 			}
 
-			.utah {
+			#utah {
 				width: 243px;
 				height: 209px;
-				background-image: url("/assets/unplug/teapot.png");
+				background-image: url("/assets/unplug/teapot.png?v=2");
 				margin-top: calc(58vh + 5px);
 				margin-left: calc(62vw + 10px);
+			}
+
+			#utah>canvas {
+				margin-top: 24px;
+				margin-left: 5px;
 			}
 
 			.twitterwindow {
@@ -269,7 +274,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php'; ?>
 				overflow: hidden;
 			}
 		</style>
-		<link href="https://fonts.googleapis.com/css?family=Quicksand:500,700&display=swap" rel="stylesheet">
+		<link rel="StyleSheet" id="quicksand" type="text/css" href="">
 	</head>
 	<body>
 		<div class="mobile-wrapper" id="mobile">
@@ -326,38 +331,37 @@ also i have a <a href="https://twitter.com/unplugred/">twitter</a>
 				<a class="object" href="https://unplugred.itch.io/"></a>
 			</div>
 
-			<div class="clock window">
+			<div id="clock" class="window">
 				<a class="close" href="javascript:void(0)" title="close"></a>
 				<canvas id="clockcanvas"></canvas>
 			</div>
 
-			<div class="grid window">
+			<div id="grid" class="window">
 				<a class="close" href="javascript:void(0)" title="close"></a>
 			</div>
 
-			<div class="utah window">
+			<div id="utah" class="window">
 				<a class="close" href="javascript:void(0)" title="close"></a>
 			</div>
 
 			<div id="copyrightstuff">© Ari Hanan 2018-2019 | <a href="/privacy-policy">privacy policy</a> | &lt;3</div>
 		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-		<script>
+		<script type="text/javascript">
 			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-				|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+				|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)))
 				document.getElementById("mobile").style.display = "table";
-			}
-		else{
+			else
 				document.getElementById("desktop").style.display = "block";
-			}
 
 			function Switch(){
 				document.getElementById("mobile").style.display = "none";
 				document.getElementById("desktop").style.display = "block";
+				render();
 			}
-
+		</script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+		<script type="text/javascript">
 			var closedwindows = 0;
 			var biowindow = $("#hhh")[0];
 			var biocontent = $("#aboutcontent")[0];
@@ -442,6 +446,60 @@ also i have a <a href="https://twitter.com/unplugred/">twitter</a>
 				for(i = 0; i != imageData.data.length; i++)
 					imageData.data[i] = (imageData.data[i] >> 7) * 0xFF;
 				ctx.putImageData(imageData, 0, 0);
+			}
+		</script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/threejs/r84/three.min.js"></script>
+		<script type="text/javascript">
+			var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera(22.619865, 233 / 180, 0.1, 1000);
+			var renderer = new THREE.WebGLRenderer({antialias: false});
+			renderer.setClearColor(0x000000, 0);
+			renderer.setSize(233, 180);
+			renderer.stencil = false;
+			renderer.depth = false;
+			renderer.premultipliedAlpha = false;
+			renderer.precision = "lowp";
+
+			var loader = new THREE.TextureLoader();
+			var bg = loader.load("/assets/unplug/checkers.png");
+			bg.wrapS = THREE.RepeatWrapping;
+			bg.wrapT = THREE.RepeatWrapping;
+			bg.repeat.set(14.5625, 11.25);
+			scene.background = bg;
+
+			camera.position.set(6.24413, 5.60956, 10.5715);
+			camera.rotation.set(-0.4784, 0.45708, 0.2294);
+
+			var material = new THREE.MeshNormalMaterial();
+			var teapot;
+			loader = new THREE.ObjectLoader();
+			loader.load("/assets/unplug/pot.json", function ( obj ) {
+				obj.material = material;
+				scene.add(obj);
+				teapot = obj;
+			});
+
+			function animate() {
+				requestAnimationFrame(animate);
+				renderer.render(scene, camera);
+			}
+			animate();
+
+			document.getElementById("utah").appendChild(renderer.domElement);
+			var canvasdom = $("#utah>canvas");
+
+			document.onmousemove = function(e){
+				if(scene.children.length == 1)
+				{
+					var offset = canvasdom.offset();
+					teapot.rotation.x = (e.pageY - offset.top - 90)*.001;
+					teapot.rotation.y = (e.pageX - offset.left - 116.5)*.001;
+				}
+			}
+
+			window.onload = function()
+			{
+				document.getElementById('quicksand').href = "https://fonts.googleapis.com/css?family=Quicksand:500,700&display=swap";
 			}
 		</script>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php'; ?>
