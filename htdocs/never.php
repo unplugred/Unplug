cf390b5234cb38dfabd2d6e879d7fb97284f253b
red <arihanan@protonmail.com>
2019-09-28 18:54:59 +0300

    added mailing list

--- htdocs/never.php
+++ htdocs/never.php
@@ -1,23 +1,22 @@
-	<?php
-	$color = "#32363B";
-	$title = "never";
-	include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
-			<style>
+<?php
+$color = "#32363B";
+$title = "never";
+include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
+		<style>
+			body {
+				background-color:#32363B;
+			}
 
-				body{
-					background-color:#32363B;
-				}
-
-				.mainimage {
-					margin-top: calc(50vh - 260px);
-					margin-left: calc(50vw - 49px);
-					display: block;
-					width: 98px;
-					height: 520px;
-					background-image: url("<?php echo $assets ?>/never.png");
-				}
-			</style>
-		</head>
-		<body>
-			<div class="mainimage" title="but i never said that"></div>
-	<?php include $_SERVER['DOCUMENT_ROOT'].'/access/footer.php'; ?>
+			.mainimage {
+				margin-top: calc(50vh - 260px);
+				margin-left: calc(50vw - 49px);
+				display: block;
+				width: 98px;
+				height: 520px;
+				background-image: url("<?php echo $assets ?>/never.png");
+			}
+		</style>
+	</head>
+	<body>
+		<a class="mainimage" title="but i never said that" href="/mail"></a>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/access/footer.php'; ?>
