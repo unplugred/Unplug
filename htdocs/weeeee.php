f5ed2342762439662930217bcd19a7254e08b0c7
melody <melody@unplug.red>
2019-11-19 17:51:25 +0200

    converted weeeee to canvas

--- htdocs/weeeee.php
+++ htdocs/weeeee.php
@@ -15,107 +15,113 @@ include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
 				image-rendering: pixelated;
 			}
 
-			#title {
+			#canva {
 				position: fixed;
-				width: 359px;
-				height: 19px;
-				background-image: url(<?php echo $assets ?>/weeeee/title.png);
-				background-size: cover;
-				background-position: top right;
-				top: 50vh;
-				left: calc(50vw - 180px);
-			}
-
-			.block{
-				transform: translateX(-50%) translateY(-50%);
-				box-shadow: 5px 5px 0px 0px rgba(0,0,0,1);
-				position: fixed;
-			}
-
-			#appendme{
-				position: relative;
-			}
-
-			#weeeee{
-				width: 17px;
-				position: fixed;
-				display: flex;
-				flex-direction: column;
-    			margin-left: -9px;
-			}
-
-			.t{
-				width: 17px;
-			}
-
-			#w{
-				background-image: url("<?php echo $assets ?>/weeeee/w.png");
-			}
-
-			#e{
-				background-image: url("<?php echo $assets ?>/weeeee/e.png");
-			}
-
-			#m{
-				background-image: url("<?php echo $assets ?>/weeeee/m.png");
+				top: 0;
+				left: 0;
 			}
 		</style>
 	</head>
 	<body>
-		<div id="appendme"></div>
-		<div id="weeeee">
-			<div id="w" class="t"></div>
-			<div id="e" class="t"></div>
-			<div id="m" class="t"></div>
-		</div>
-		<div id="title"></div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<img style="display: none" id="w" src="<?php echo $assets ?>/weeeee/w.png">
+		<img style="display: none" id="e" src="<?php echo $assets ?>/weeeee/e.png">
+		<img style="display: none" id="m" src="<?php echo $assets ?>/weeeee/m.png">
+		<img style="display: none" id="title" src="<?php echo $assets ?>/weeeee/title.png">
+		<img style="display: none" id="0a" src="<?php echo $assets ?>/weeeee/0a.png?v=2">
+		<img style="display: none" id="0b" src="<?php echo $assets ?>/weeeee/0b.png?v=2">
+		<img style="display: none" id="1a" src="<?php echo $assets ?>/weeeee/1a.png?v=2">
+		<img style="display: none" id="1b" src="<?php echo $assets ?>/weeeee/1b.png?v=2">
+		<img style="display: none" id="2" src="<?php echo $assets ?>/weeeee/2.png?v=2">
+		<img style="display: none" id="3" src="<?php echo $assets ?>/weeeee/3.png?v=2">
+		<img style="display: none" id="4" src="<?php echo $assets ?>/weeeee/4.png?v=2">
+		<img style="display: none" id="5" src="<?php echo $assets ?>/weeeee/5.png?v=2">
+		<img style="display: none" id="6" src="<?php echo $assets ?>/weeeee/6.png?v=2">
+		<img style="display: none" id="7a" src="<?php echo $assets ?>/weeeee/7a.png?v=2">
+		<img style="display: none" id="7b" src="<?php echo $assets ?>/weeeee/7b.png?v=2">
+		<img style="display: none" id="7c" src="<?php echo $assets ?>/weeeee/7c.png?v=2">
+		<img style="display: none" id="7d" src="<?php echo $assets ?>/weeeee/7d.png?v=2">
+		<canvas id="canva"></canvas>
 		<script>
-			function makeEven(val){
-				return Math.floor(val * .5) * 2;
-			}
-
-			var scrollspeed = .1;
-			var weespeed = .5;
-
+			var canvas = document.getElementById("canva");
+			var ctx = canvas.getContext("2d");
+			var letters = [
+				document.getElementById("w"),
+				document.getElementById("e"),
+				document.getElementById("m")];
 			var title = document.getElementById("title");
-			var weediv = document.getElementById("weeeee");
-			var weew = document.getElementById("w");
-			var weee = document.getElementById("e");
-			var weem = document.getElementById("m");
+			var patterns = [
+				document.getElementById("0a"),
+				document.getElementById("0b"),
+				document.getElementById("1a"),
+				document.getElementById("1b"),
+				document.getElementById("2"),
+				document.getElementById("3"),
+				document.getElementById("4"),
+				document.getElementById("5"),
+				document.getElementById("6"),
+				document.getElementById("7a"),
+				document.getElementById("7b"),
+				document.getElementById("7c"),
+				document.getElementById("7d")];
+
+			var scrollspeed = .15;
+			var weespeed = .5;
 			var weetop, weeanchor;
 			var weestage = 6;
 			var weeheight = 999999;
 			var weeend = Math.random() * 500 + 500;
-			var h = $('body').height() * .5;
 			var scrollTop = document.documentElement ? document.documentElement.scrollTop : document.body.scrollTop;
 			var prevscroll = 0;
-			var isMobile = false; //initiate as false
-			// device detection
-			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
-				isMobile = true;
+
+			window.onload = function(){
+				redraw();
+			};
+			window.onresize = function(){ redraw(); };
+
+			function redraw()
+			{
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+				draw();
 			}
 
-			function Blox(){
-				this.div = document.createElement("div");
+			function makeEven(val)
+			{
+				return Math.floor(val * .5) * 2;
+			}
+
+			function Blox()
+			{
 				var w = Math.random() * .9 + .05;
 				var s = Math.random() * 600 + 40;
-				this.div.style.width = makeEven(Math.min(1 - w, w) * s) + "px";
-				this.div.style.height = makeEven(Math.max(1 - w, w) * s) + "px";
-				this.position = [ Math.random() * 100, Math.random() * 10, Math.floor(Math.random() * 1024), Math.floor(Math.random() * 1024) ];
+				this.width = makeEven(Math.min(1 - w, w) * s);
+				this.height = makeEven(Math.max(1 - w, w) * s);
+
+				this.position = [ Math.random(), Math.random() * 10, Math.floor(Math.random() * 1024), Math.floor(Math.random() * 1024) ];
+
 				this.speed = Math.random() - .1;
-				this.div.style.left = this.position[0] + "vw";
+
 				var tex = Math.floor(Math.random() * 8);
-				this.scrollablebg = tex !== 3;
-				if(tex < 2) tex += Math.random() > .5 ? "a" : "b";
-				if(tex === 7) tex += Math.random() > .5 ? (Math.random() > .5 ? "a" : "b") : (Math.random() > .5 ? "c" : "d");
-				this.div.style.backgroundImage = "url(\"<?php echo $assets ?>/weeeee/" + tex + ".png\")";
-				this.div.className = "block";
+				this.bg = tex;
+				switch(tex)
+				{
+					case 0:
+						this.bg = (Math.random() > 0.5) ? 1 : 0;
+						break;
+					case 1:
+						this.bg = (Math.random() > 0.5) ? 3 : 2;
+						break;
+					case 7:
+						this.bg = Math.floor(Math.random() * 4) + 9;
+						break;
+					default:
+						this.bg += 2;
+						break;
+				}
 			}
 
 			var bloxes = [];
-			for (x = 0; x < (isMobile ? 25 : 100); x += 1)
-				document.getElementById("appendme").appendChild((bloxes[x] = new Blox()).div);
+			for (x = 0; x < 200; x += 1) bloxes[x] = new Blox();
 
 			function update() {
 				var scrolldif = scrollTop;
@@ -130,90 +136,50 @@ include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
 				}
 			}
 
-			//window.onscroll = update;
 			setInterval(update, 33);
 
-			function GnrtWee(){
-				weeend = Math.random()*200 + 100;
-				weediv.style.left = (Math.random()*100) + "vw";
-				weestage = 1;
-				weetop = Math.random()*.4 + .2;
-				weeanchor = scrollTop;
-				weediv.style.top = (weeanchor - window.innerHeight*weetop) + "px";
-				weew.style.height =
-				weee.style.height =
-				weem.style.height =
-				weew.style.marginTop =
-				weee.style.marginTop =
-				weem.style.marginTop = 0;
-			}
-
-			function draw(){
-				title.style.top = (window.innerHeight*.5 - scrollTop) + "px";
-
-				var boxpos;
-				for(x = 0; x < bloxes.length; x++)
+			function drawbox(x,y,w,h,bg,bgx,bgy)
+			{
+				ctx.fillRect(x + 5,y + 5,w,h);
+				if(y > canvas.height || y < -h) return;
+				var ax = 0;
+				var ay = 0;
+				var bx = 0;
+				var by = 0;
+				var cx = 0;
+				var cy = 0;
+				for(px = bgx; px < w; px += patterns[bg].width)
 				{
-					boxpos = (bloxes[x].position[1] * window.innerHeight + (scrollTop - $('body').height()*.5) * bloxes[x].speed - scrollTop);
-					bloxes[x].div.style.top = boxpos + "px";
-					bloxes[x].div.style.backgroundPosition = (bloxes[x].position[2]) + "px " + (bloxes[x].position[3] - boxpos*(bloxes[x].scrollablebg ? .5 : 1)) + "px";
-				}
-
-				weeheight += (weestage <= 3) ? Math.max(prevscroll*weespeed*1.5, 0) : 20;
-				var afterclamp = (weeheight - (weeheight % 22));
-				weediv.style.top = ((weeanchor - scrollTop)*weespeed + window.innerHeight*weetop) + "px";
-				switch(weestage){
-					case 1:
-						weew.style.height = afterclamp + "px";
-						break;
-					case 2:
-						weee.style.height = afterclamp + "px";
-						break;
-					case 3:
-						weem.style.height = afterclamp + "px";
-						break;
-					case 4:
-						weew.style.height = (weeend - afterclamp) + "px";
-						weew.style.marginTop = afterclamp + "px";
-						break;
-					case 5:
-						weee.style.height = (weeend - afterclamp) + "px";
-						weee.style.marginTop = afterclamp + "px";
-						break;
-					case 6:
-						weem.style.height = (weeend - afterclamp) + "px";
-						weem.style.marginTop = afterclamp + "px";
-						break;
-				}
-				if(weeheight > weeend){
-					weeheight = 0;
-					switch(weestage){
-						case 3:
-							weeend = parseInt(weew.style.height, 10);
-							weestage++;
-							break;
-						case 4:
-							weeend = parseInt(weee.style.height, 10);
-							weestage++;
-							break;
-						case 5:
-							weeend = parseInt(weem.style.height, 10);
-							weestage++;
-							break;
-						case 6:
-							weestage++;
-							break;
-						case 7:
-							if(prevscroll > 0) GnrtWee();
-							break;
-						default:
-							weeend = Math.random()*200 + 100;
-							weestage++;
-							break;
+					for(py = bgy; py < h; py += patterns[bg].height)
+					{
+						ax = -Math.min(px,0);
+						ay = -Math.min(py,0);
+						bx = Math.min(patterns[bg].width , w - px - ax);
+						by = Math.min(patterns[bg].height, h - py - ay);
+						ctx.drawImage(patterns[bg], ax, ay, bx, by, x+Math.max(px,0), y+Math.max(py,0), bx, by);
 					}
 				}
 			}
 
-			draw();
+			function draw()
+			{
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+				var boxpos;
+				for(x = 0; x < bloxes.length; x++)
+				{
+					boxpos = (bloxes[x].position[1] * window.innerHeight + (scrollTop - window.innerHeight*5) * bloxes[x].speed - scrollTop);
+					drawbox(
+						bloxes[x].position[0] * canvas.width - (bloxes[x].width*.5),
+						boxpos - (bloxes[x].height*.5),
+						bloxes[x].width,
+						bloxes[x].height,
+						bloxes[x].bg,
+						-(bloxes[x].position[2] % patterns[bloxes[x].bg].width),
+						-((bloxes[x].position[3] + boxpos*(bloxes[x].bg !== 5 ? .5 : 1) + 2048) % patterns[bloxes[x].bg].width));
+				}
+
+				ctx.drawImage(title, window.innerWidth*.5 - 180, window.innerHeight*.5 - scrollTop);
+			}
 		</script>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/access/footer.php'; ?>
