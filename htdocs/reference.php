6973b8825b70008a501bfce9e302a945ddba73b6
red <arihanan@pm.me>
2019-11-20 20:52:57 +0200

    reference page

---
+++ htdocs/reference.php
@@ -0,0 +1,22 @@
+<?php
+$color = "#000CFF";
+$title = "reference";
+include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
+		<style>
+			body {
+				background-color:#000CFF;
+			}
+
+			.mainimage {
+				margin-top: calc(40vh - 34px);
+				margin-left: calc(50vw - 61px);
+				display: block;
+				width: 122px;
+				height: 68px;
+				background-image: url("<?php echo $assets ?>/reference.gif");
+			}
+		</style>
+	</head>
+	<body>
+		<div class="mainimage" title="hexagon sun called"></div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/access/footer.php'; ?>
