552571050d532f69e2f73e07826c99c20977eff2
red <arihanan@pm.me>
2019-11-21 09:28:45 +0200

    inlined all css in games

--- htdocs/planet/index.php
+++ htdocs/planet/index.php
@@ -8,7 +8,51 @@ $icon = "/planet/favicon.ico";
 $ogimage = "/planet/cover.png";
 $css = false;
 include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
-		<link rel="StyleSheet" href="style.css">
+		<style>
+			canvas {
+				position: absolute;
+			}
+
+			#gameContainer {
+				width: 100vw;
+				height: 100vw;
+				max-width: 100vh;
+				max-height: 100vh;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+
+			body {
+				width: 100vw;
+				height: 100vh;
+				margin: 0;
+				overflow: hidden;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				background-color: black;
+			}
+
+			html {
+				width: 100vw;
+				height: 100vh;
+				margin: 0;
+			}
+
+			#full {
+				background-color: black;
+			}
+
+			#progress
+			{
+				display: flex;
+				width: 130px;
+				height: 15px;
+				border: 8px black solid;
+				border-radius: 50px;
+			}
+		</style>
 		<script src="UnityProgress.js"></script>
 		<script src="<?php echo $assets ?>/UnityLoader.js"></script>
 		<script>
