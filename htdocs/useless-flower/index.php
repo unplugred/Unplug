4635ed4842522e52345bbd919f9a96695492ae21
melody <melody@unplug.red>
2019-11-23 13:35:45 +0200

    useless flower bar removed play added script unify

--- htdocs/useless-flower/index.php
+++ htdocs/useless-flower/index.php
@@ -26,42 +26,40 @@ include $_SERVER['DOCUMENT_ROOT'].'/access/header.php'; ?>
 				margin: 0;
 			}
 
-			#full {
-				background-image: url("full.png");
-				background-position: left;
-			}
-
-			#empty{
-				width: 5px;
-				background-image: url("full.png");
-				background-position: right;
-			}
-
-			#progress {
-				display: flex;
-				width: 141px;
-				height: 7px;
-				padding: 147px;
-				background-image: url(box.png);
-				background-repeat: no-repeat;
-				background-position: center center;
-			}
-
 			#gameContainer {
 				width: 100%;
 				height: 100%;
 				display: flex;
 				justify-content: center;
 				align-items: center;
-				background-image: url("bg.png") !important;
-				background-repeat: repeat;
+				background-image: url("play.png"), url("bg.png");
+				background-repeat: no-repeat, repeat;
+				background-position: center center, left top;
+			}
+
+			#play {
+				width: 200px;
+				height: 57px;
+			}
+
+			.loading {
+				background-image: url("loading.gif"), url("play.png"), url("bg.png") !important;
+				background-repeat: no-repeat, no-repeat, repeat !important;
+				background-position: center center, center center, left top !important;
 			}
 		</style>
-		<script src="UnityProgress.js"></script>
+		<link rel="prefetch" href="loading.gif" />
+	</head>
+	<body>
+		<div id="gameContainer"><a id="play" href="javascript:void(0)" onclick="play()"></a></div>
+		<script src="<?php echo $assets ?>/UnityProgress.js"></script>
 		<script src="<?php echo $assets ?>/UnityLoader.js"></script>
 		<script>
-			var unityInstance = UnityLoader.instantiate("gameContainer", "Build/uselessflower.json", {onProgress: UnityProgress});
+			function play()
+			{
+				document.getElementById("play").style.display = "none";
+				document.getElementById("gameContainer").className = "loading";
+				var unityInstance = UnityLoader.instantiate("gameContainer", "Build/uselessflower.json", {onProgress: UnityProgress});
+			}
 		</script>
-	</head>
-	<body><div id="gameContainer"></div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/access/footer.php'; ?>
