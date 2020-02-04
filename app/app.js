e75e527fc93add6dd9ab5f7e82fe273bd90f926e
red <arihanan@pm.me>
2020-02-04 21:30:38 +0200

    triple equal-ed everything

--- app/app.js
+++ app/app.js
@@ -67,7 +67,7 @@ app.get('*', function(req, res, next){
 		}
 	}
 	//normal website
-	else if(req.hostname === 'www.unplug.red' || req.hostname == 'unplug.red' || req.hostname === 'localhost') {
+	else if(req.hostname === 'www.unplug.red' || req.hostname === 'unplug.red' || req.hostname === 'localhost') {
 		if(req.url.startsWith("/assets") || req.url.startsWith("/dreambuster"))
 		{
 			res.status(404).render('pages/404',{assets:global.assets,host:req.hostname,version:version});
