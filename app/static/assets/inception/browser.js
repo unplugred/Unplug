b4fa1a0172be30d3acf94235923e24d322facf8e
red <arihanan@pm.me>
2020-02-05 11:22:38 +0200

    made browser address line more robust

--- app/static/assets/inception/browser.js
+++ app/static/assets/inception/browser.js
@@ -2,6 +2,7 @@ var iframe = document.getElementById("iframe");
 var input = document.getElementById("input");
 var back = document.getElementById("back");
 var front = document.getElementById("front");
+var go = document.getElementById("go");
 var rainbow = document.getElementById("rainbow");
 var titletext = document.getElementById("titletext");
 var hostt = "https://         /";
@@ -9,6 +10,7 @@ var hostt = "https://         /";
 var historyy = [];
 var unhistory = [];
 var current = validate(iframe.src);
+var valid = true;
 input.value = hostt + current;
 prevvalue = current;
 rainbow.style.width = Math.ceil(Math.max(Math.min(input.value.length, hostt.length - 1) - 8, 0)*6.6) + "px";
@@ -16,6 +18,7 @@ rainbow.style.backgroundPositionX = (Math.random()*100) + "%";
 
 function changeurl()
 {
+	if(!valid) return;
 	v = validate(input.value.substr(hostt.length));
 	if(v !== current)
 	{
@@ -41,6 +44,7 @@ function updateurl(urll)
 		front.className = "off";
 	}
 	input.value = hostt + current;
+	updatego();
 }
 
 function validate(v)
@@ -99,21 +103,39 @@ function frontt()
 	back.className = null;
 }
 
+function updatego() {
+	rainbow.style.width = Math.ceil(Math.max(Math.min(input.value.length, hostt.length - 1) - 8, 0)*6.6) + "px";
+	rainbow.style.backgroundPositionX = (Math.random()*100) + "%";
+
+	if(valid) {
+		if(input.value.length < hostt.length)
+		{
+			valid = false;
+			go.className = "off";
+		}
+	} else {
+		if(input.value.length >= hostt.length)
+		{
+			valid = true;
+			go.className = null;
+		}
+	}
+}
+
 input.addEventListener('keydown', function(event)
 {
 	if(event.which === 13) changeurl();
 	else setTimeout(function() {
 		if(input.value === prevvalue) return;
-		if(input.value.length <= hostt.length)
-		{
+		if(input.value.length <= hostt.length) {
 			input.value = hostt.substr(0, input.value.length);
 			prevvalue = input.value;
-			rainbow.style.width = Math.ceil(Math.max(Math.min(input.value.length, hostt.length - 1) - 8, 0)*6.6) + "px";
 		}
 		else if(input.value.startsWith(hostt))
 			prevvalue = input.value;
 		else
 			input.value = prevvalue;
-		rainbow.style.backgroundPositionX = (Math.random()*100) + "%";
-	});
+
+		updatego();
+	}, 1);
 });
