9eec053a5b81750c43f2ddab8d4342741c487703
melody <melody@unplug.red>
2020-04-02 10:09:33 +0300

    made paths better + fixed bash version

--- start.sh
+++ start.sh
@@ -9,20 +9,19 @@ then
 		echo "This script must be run as root" 1>&2
 		exit 1
 	fi
-	cd app
 
 	read -p "[S]tart/[R]estart? " -n 1 -r
 	echo
 	if [[ $REPLY =~ ^[Ss]$ ]]
 	then
-		pm2 start servdebug.js --time -l "../logs/combined.log" -o "../logs/out.log" -e "../logs/"
+		pm2 start "./app/servdebug.js" --time -l "./logs/combined.log" -o "./logs/out.log" -e "./logs/error.log"
 		pm2 log
 		exit 1
 	fi
 
 	if [[ $REPLY =~ ^[Rr]$ ]]
 	then
-		pm2 restart servdebug.js --time -l "../logs/combined.log" -o "../logs/out.log" -e "../logs/"
+		pm2 restart "./app/servdebug.js" --time -l "./logs/combined.log" -o "./logs/out.log" -e "./logs/error.log"
 		pm2 log
 		exit 1
 	fi
@@ -40,20 +39,19 @@ then
 	fi
 
 	git pull
-	cd app
 
 	read -p "[S]tart/[R]estart? " -n 1 -r
 	echo
 	if [[ $REPLY =~ ^[Ss]$ ]]
 	then
-		pm2 start servprod.js --time -l "../logs/combined.log" -o "../logs/out.log" -e "../logs/"
+		pm2 start "./app/servprod.js" --time -l "./logs/combined.log" -o "./logs/out.log" -e "./logs/error.log"
 		pm2 log
 		exit 1
 	fi
 
 	if [[ $REPLY =~ ^[Rr]$ ]]
 	then
-		pm2 restart servprod.js --time -l "../logs/combined.log" -o "../logs/out.log" -e "../logs/"
+		pm2 restart "./app/servprod.js" --time -l "./logs/combined.log" -o "./logs/out.log" -e "./logs/error.log"
 		pm2 log
 		exit 1
 	fi
@@ -64,19 +62,18 @@ fi
 if [[ $REPLY =~ ^[Ee]$ ]]
 then
 	echo ELECTRON MODE
-	cd app
 
 	read -p "[P]roduction/[D]ebug? " -n 1 -r
 	echo
 	if [[ $REPLY =~ ^[Pp]$ ]]
 	then
-		electron electprod.js
+		electron "./app/electprod.js"
 		exit 1
 	fi
 
 	if [[ $REPLY =~ ^[Dd]$ ]]
 	then
-		electron electdebug.js
+		electron "./app/electdebug.js"
 		exit 1
 	fi
 
