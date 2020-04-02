6a373bc87081fadbd133f348825e842a6c68c3d5
melody <melody@unplug.red>
2020-04-02 10:27:44 +0300

    added logging to monthly.sh

--- monthly.sh
+++ monthly.sh
@@ -3,10 +3,16 @@ if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
 fi
+echo "--FLUSHING LOGS"
 pm2 flush
+echo "--UPDATING NPM"
 npm install -g npm
+echo "--UPDATING PM2"
 npm install pm2@latest -g
 pm2 update
+echo "--UPDATING UBUNTU PACKAGES"
 apt-get update
+echo "--UPGRADING UBUNTU PACKAGES"
 apt-get upgrade
+echo "--BYE BYE"
 reboot
