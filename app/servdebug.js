664f8dc1a0ced34a7c376fa900c4d86403b046d2
red <arihanan@pm.me>
2020-04-04 00:17:16 +0300

    added the ecosystems thing and moved to global variables

--- app/servdebug.js
+++
@@ -1,4 +0,0 @@
-global.portt = 80;
-global.protocol = "http://";
-global.domain = "localhost";
-require('./server.js');
--- app/servprod.js
+++
@@ -1,4 +0,0 @@
-global.portt = 80;
-global.protocol = "https://";
-global.domain = "unplug.red";
-require('./server.js');
