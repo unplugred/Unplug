0cd1cbd00def8037b1d47357f1b0ff591390f713
melody <melody@unplug.red>
2020-04-15 07:52:39 +0300

    fixed bug in dreambuster

--- dreambuster/server.js
+++ dreambuster/server.js
@@ -30,8 +30,8 @@ app.use((req, res, next) => {
 	if(req.path === "/halloffame")
 		return fs.readdir(__dirname + '/static/hof', (err, files) => {
 			if(err) throw err;
-			else return res.render('halloffame.ejs', {assets:global.protocol + req.headers.host,host:global.protocol + req.headers.host,version:version,files:files})});
-	else return res.render('index.ejs', {assets:global.protocol + req.headers.host,host:global.protocol + req.headers.host,version:version});
+			else return res.render('halloffame.ejs', {assets:global.protocol + req.headers.host,host:global.protocol + req.headers.host,files:files})});
+	else return res.render('index.ejs', {assets:global.protocol + req.headers.host,host:global.protocol + req.headers.host});
 });
 
 app.listen(6661, function(){
