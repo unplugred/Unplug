bd1d7d7d53ce0f184bbd19bf41c8c73059d79004
red <AriHanan@pm.me>
2021-01-01 00:39:04 +0200

    added vst subdomain

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -33,5 +33,22 @@ module.exports = {
 		env_production: {
 			NODE_ENV: 'production'
 		}
+	},{
+		name: 'vst',
+		script: './vst/server.js',
+		autorestart: true,
+		watch: false,
+		max_memory_restart: '1G',
+		error_file: './logs/error.log',
+		out_file: './logs/out.log',
+		log_file: './logs/combined.log',
+		time: true,
+		combine_logs: true,
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
 	}]
 };
