9c214841cc550ec15e06f18bb092635e58e011dd
melody <melody@unplug.red>
2021-05-19 05:09:41 +0300

    added psycho stellar to stats

--- dreambuster/static/score.js
+++ dreambuster/static/score.js
@@ -3,221 +3,73 @@ var dataa = [
 		number: 43,
 		name: "Car Destruction Racer XIV",
 		submissions: 1748,
-		overall: {
-			rating: 4.111,
-			amount: 38,
-			place: 52
-		},
-		fun: {
-			rating: 4,
-			amount: 39,
-			place: 54
-		},
-		innovation: {
-			rating: 4.139,
-			amount: 38,
-			place: 25
-		},
-		theme: {
-			rating: 4.186,
-			amount: 37,
-			place: 78
-		},
-		graphics: {
-			rating: 4.27,
-			amount: 39,
-			place: 123
-		},
-		audio: {
-			rating: 4.143,
-			amount: 37,
-			place: 29
-		},
-		humor: {
-			rating: 3.958,
-			amount: 38,
-			place: 72
-		},
-		mood: {
-			rating: 3.958,
-			amount: 38,
-			place: 102
-		}
+		overall: { rating: 4.111, amount: 38, place: 52 },
+		fun: { rating: 4, amount: 39, place: 54 },
+		innovation: { rating: 4.139, amount: 38, place: 25 },
+		theme: { rating: 4.186, amount: 37, place: 78 },
+		graphics: { rating: 4.27, amount: 39, place: 123 },
+		audio: { rating: 4.143, amount: 37, place: 29 },
+		humor: { rating: 3.958, amount: 38, place: 72 },
+		mood: { rating: 3.958, amount: 38, place: 102 }
 	},{
 		number: 44,
 		name: "Attack on Jerry",
 		submissions: 1817,
-		overall: {
-			rating: 3.808,
-			amount: 41,
-			place: 185
-		},
-		fun: {
-			rating: 3.628,
-			amount: 41,
-			place: 245
-		},
-		innovation: {
-			rating: 3.039,
-			amount: 40,
-			place: 610
-		},
-		theme: {
-			rating: 3.603,
-			amount: 41,
-			place: 350
-		},
-		graphics: {
-			rating: 3.949,
-			amount: 41,
-			place: 310
-		},
-		audio: {
-			rating: 3.438,
-			amount: 42,
-			place: 318
-		},
-		humor: {
-			rating: 3.808,
-			amount: 41,
-			place: 97
-		},
-		mood: {
-			rating: 3.538,
-			amount: 41,
-			place: 360
-		}
+		overall: { rating: 3.808, amount: 41, place: 185 },
+		fun: { rating: 3.628, amount: 41, place: 245 },
+		innovation: { rating: 3.039, amount: 40, place: 610 },
+		theme: { rating: 3.603, amount: 41, place: 350 },
+		graphics: { rating: 3.949, amount: 41, place: 310 },
+		audio: { rating: 3.438, amount: 42, place: 318 },
+		humor: { rating: 3.808, amount: 41, place: 97 },
+		mood: { rating: 3.538, amount: 41, place: 360 }
 	},{
 		number: 45,
 		name: "Bruno's Mind Control Project",
 		submissions: 1883,
-		overall: {
-			rating: 3.643,
-			amount: 37,
-			place: 343
-		},
-		fun: {
-			rating: 3.529,
-			amount: 37,
-			place: 352
-		},
-		innovation: {
-			rating: 3.6,
-			amount: 37,
-			place: 262
-		},
-		theme: {
-			rating: 3.143,
-			amount: 37,
-			place: 703
-		},
-		graphics: {
-			rating: 3.456,
-			amount: 36,
-			place: 605
-		},
-		audio: {
-			rating: 3.4,
-			amount: 37,
-			place: 343
-		},
-		humor: {
-			rating: 3.129,
-			amount: 33,
-			place: 357
-		},
-		mood: {
-			rating: 3.529,
-			amount: 36,
-			place: 399
-		}
+		overall: { rating: 3.643, amount: 37, place: 343 },
+		fun: { rating: 3.529, amount: 37, place: 352 },
+		innovation: { rating: 3.6, amount: 37, place: 262 },
+		theme: { rating: 3.143, amount: 37, place: 703 },
+		graphics: { rating: 3.456, amount: 36, place: 605 },
+		audio: { rating: 3.4, amount: 37, place: 343 },
+		humor: { rating: 3.129, amount: 33, place: 357 },
+		mood: { rating: 3.529, amount: 36, place: 399 }
 	},{
 		number: 46,
 		name: "a Very Talented Worm",
 		submissions: 3576,
-		overall: {
-			rating: 3.75,
-			amount: 20,
-			place: 614
-		},
-		fun: {
-			rating: 3.722,
-			amount: 20,
-			place: 457
-		},
-		innovation: {
-			rating: 3.528,
-			amount: 20,
-			place: 742
-		},
-		theme: {
-			rating: 3.361,
-			amount: 20,
-			place: 1815
-		},
-		graphics: {
-			rating: 3.667,
-			amount: 20,
-			place: 1175
-		},
-		audio: {
-			rating: 3.528,
-			amount: 20,
-			place: 670
-		},
-		humor: {
-			rating: 3.722,
-			amount: 20,
-			place: 309
-		},
-		mood: {
-			rating: 3.528,
-			amount: 20,
-			place: 953
-		}
+		overall: { rating: 3.75, amount: 20, place: 614 },
+		fun: { rating: 3.722, amount: 20, place: 457 },
+		innovation: { rating: 3.528, amount: 20, place: 742 },
+		theme: { rating: 3.361, amount: 20, place: 1815 },
+		graphics: { rating: 3.667, amount: 20, place: 1175 },
+		audio: { rating: 3.528, amount: 20, place: 670 },
+		humor: { rating: 3.722, amount: 20, place: 309 },
+		mood: { rating: 3.528, amount: 20, place: 953 }
 	},{
 		number: 47,
 		name: "Record Runner",
-		submissions: 2404,
-		overall: {
-			rating: 4.222,
-			amount: 20,
-			place: 53
-		},
-		fun: {
-			rating: 4.167,
-			amount: 20,
-			place: 51
-		},
-		innovation: {
-			rating: 3.944,
-			amount: 20,
-			place: 175
-		},
-		theme: {
-			rating: 4.361,
-			amount: 20,
-			place: 57
-		},
-		graphics: {
-			rating: 4.056,
-			amount: 20,
-			place: 344
-		},
-		audio: {
-			rating: 4.528,
-			amount: 20,
-			place: 12
-		},
-		humor: {
-			rating: 3.861,
-			amount: 20,
-			place: 148
-		},
-		mood: {
-			rating: 4.083,
-			amount: 20,
-			place: 131
-		}
+		submissions: 2406,
+		overall: { rating: 4.222, amount: 20, place: 53 },
+		fun: { rating: 4.167, amount: 20, place: 51 },
+		innovation: { rating: 3.944, amount: 20, place: 175 },
+		theme: { rating: 4.361, amount: 20, place: 57 },
+		graphics: { rating: 4.056, amount: 20, place: 344 },
+		audio: { rating: 4.528, amount: 20, place: 12 },
+		humor: { rating: 3.861, amount: 20, place: 148 },
+		mood: { rating: 4.083, amount: 20, place: 131 }
+	},{
+		number: 48,
+		name: "Psycho Stellar",
+		submissions: 2721,
+		overall: { rating: 3.762, amount: 23, place: 515 },
+		fun: { rating: 3.31, amount: 23, place: 967 },
+		innovation: { rating: 3.786, amount: 23, place: 304 },
+		theme: { rating: 2.857, amount: 23, place: 1674 },
+		graphics: { rating: 4.167, amount: 23, place: 350 },
+		audio: { rating: 3.857, amount: 23, place: 295 },
+		humor: { rating: 3.158, amount: 21, place: 544 },
+		mood: { rating: 4.19, amount: 23, place: 148 }
 	}
 ];
