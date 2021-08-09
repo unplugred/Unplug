9822b4f909d2e4ec499bc2245fbe246d875c51f1
melody <melody@unplug.red>
2021-08-09 15:42:04 +0300

    updated monthly.sh

--- monthly.sh
+++ monthly.sh
@@ -3,13 +3,13 @@ if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
 fi
-echo "--FLUSHING LOGS"
-pm2 flush
-echo "--UPDATING NPM"
-npm install -g npm@latest
 echo "--UPDATING PM2"
 npm install -g pm2@latest
 pm2 update
+echo "--UPDATING NPM"
+npm install -g npm@latest
+echo "--FLUSHING LOGS"
+pm2 flush
 echo "--UPDATING UBUNTU PACKAGES"
 apt-get update
 echo "--UPGRADING UBUNTU PACKAGES"
