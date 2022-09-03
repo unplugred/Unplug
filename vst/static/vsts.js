a613113a984d04be39fb34f5453b6f131b210a72
melody <melody@unplug.red>
2022-09-03 23:50:52 +0300

    new design complete

---
+++ vst/static/vsts.js
@@ -0,0 +1,198 @@
+var vsts = [
+	{
+		title: "About",
+		id: "about",
+		color: "orange",
+		description: `Welcome to my personal vst hell.<br/>
+i was bored so i made these, and now you too can enjoy them!<br/>
+also <a href="https://laulaulau.bandcamp.com/album/time-is-not-an-issue">check out this up and coming artist</a><br/>
+INQUIRIES:<br/>
+discord (red#3510), email (unplugred@gmail.com)`,
+		hideicon: true
+	},{
+		title: "FAQ",
+		id: "faq",
+		color: "orange",
+		description: "hi",
+		hideicon: true
+	},{
+		title: "Donate",
+		id: "donate",
+		color: "orange",
+		description: "hi",
+		hideicon: true,
+		separator: true
+	},{
+		title: "Plastic Funeral",
+		id: "pf",
+		color: "teal",
+		tagline: "Distortion/saturation that sounds like a laser beam.",
+		description: `
+forget about warmth.<br/>
+this distortion gives off a harsh and metallic sound with an FM vibe that is guaranteed to destroy any mix.<br/>
+(sounds real phat and juicy on bass)<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup_audio(0, "/pf/demo1.mp3", "Demo made by @a1https://soundcloud.com/the_real_astrodex@a2Astrodex@a3.")'>Demo 1</a>
+<a class="demo" href="javascript:void(0);" onclick='popup(1, "Plastic Funeral credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)@brThanks to @a1https://laulaulau.bandcamp.com/@a2lau@a3 for creating the factory presets as well as providing quite a bit of help.@brThanks to @a1https://marina-magnolia.tumblr.com/@a2Marina Magnolia Venegas@a3 for doing testing and QA.")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 5,
+		decoration: {width:90,height:370},
+		ui: {width:242,height:462,deg:40}
+	},{
+		title: "VU",
+		id: "vu",
+		color: "teal",
+		rating: "(Good)",
+		tagline: "VU meter for your VU metering needs.",
+		description: `
+its got:<br/>
+- very sexy user interface<br/>
+- stereo (dual) and mono (singular) modes<br/>
+- scalable ui (just drag from the corner :)<br/>
+- Unplug Industries cutting edge meter ballistic simulation<br/>
+- true RMS metering<br/>
+- adjustable reaction speed<br/>
+- adjustable nominal operation level (NoL)<br/>
+- cute lil peak led<br/>
+- almost tricks u into thinking u own analog gear<br/>
+<br/>
+i must say, that is a pretty good deal! wowza<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup(2, "VU credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/kiV10",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 10,
+		decoration: {file:"circuit.gif",width:288,height:300},
+		ui: {width:438,height:260,deg:40}
+	},{
+		title: "ClickBox",
+		id: "cb",
+		color: "teal",
+		tagline: "Randomized click generator.",
+		description: `
+generates randomized digital clicks<br/>
+made after people complained i fixed the annoying clicking issue in my first vst, plastic funeral (why??)<br/>
+<br/>
+great for creative sound design, and useful as a vca controller for other parameters and side-chaining (fruity peak controller for fl users)<br/>
+otherwise, not that useful<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup(3, "ClickBox credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/GI9s7",
+		decoration: {file:"bear.gif",width:150,height:300},
+		ui: {width:256,height:256,deg:40}
+	},{
+		title: "Pisstortion",
+		id: "ps",
+		color: "teal",
+		tagline: "Advanced sinefold distortion plugin.",
+		description: `
+forget about warmth.<br/>
+this distortion gives off a harsh and metallic sound with an FM vibe that is guaranteed to destroy any mix.<br/>
+(sounds real phat and juicy on bass)<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup_audio(4, "/pf/demo1.mp3", "Demo made by @a1https://soundcloud.com/the_real_astrodex@a2Astrodex@a3.")'>Demo 1</a>
+<a class="demo" href="javascript:void(0);" onclick='popup(5, "Plastic Funeral credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)@brThanks to @a1https://laulaulau.bandcamp.com/@a2lau@a3 for creating the factory presets as well as providing quite a bit of help.@brThanks to @a1https://marina-magnolia.tumblr.com/@a2Marina Magnolia Venegas@a3 for doing testing and QA.")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 5,
+		decoration: {width:94,height:300},
+		ui: {width:242,height:462,deg:40}
+	},{
+		title: "PNCH",
+		id: "pnch",
+		color: "teal",
+		tagline: "Weird knob that makes ur stuff tight.",
+		description: `
+forget about warmth.<br/>
+this distortion gives off a harsh and metallic sound with an FM vibe that is guaranteed to destroy any mix.<br/>
+(sounds real phat and juicy on bass)<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup_audio(6, "/pf/demo1.mp3", "Demo made by @a1https://soundcloud.com/the_real_astrodex@a2Astrodex@a3.")'>Demo 1</a>
+<a class="demo" href="javascript:void(0);" onclick='popup(7, "Plastic Funeral credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)@brThanks to @a1https://laulaulau.bandcamp.com/@a2lau@a3 for creating the factory presets as well as providing quite a bit of help.@brThanks to @a1https://marina-magnolia.tumblr.com/@a2Marina Magnolia Venegas@a3 for doing testing and QA.")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 5,
+		decoration: {width:90,height:380},
+		ui: {width:128,height:148,deg:40}
+	},{
+		title: "Red Bass",
+		id: "rb",
+		color: "teal",
+		rating: "(Good)",
+		tagline: "Low-end enchancer excellent for kicks and speech.",
+		description: `
+forget about warmth.<br/>
+this distortion gives off a harsh and metallic sound with an FM vibe that is guaranteed to destroy any mix.<br/>
+(sounds real phat and juicy on bass)<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup_audio(8, "/pf/demo1.mp3", "Demo made by @a1https://soundcloud.com/the_real_astrodex@a2Astrodex@a3.")'>Demo 1</a>
+<a class="demo" href="javascript:void(0);" onclick='popup(9, "Plastic Funeral credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)@brThanks to @a1https://laulaulau.bandcamp.com/@a2lau@a3 for creating the factory presets as well as providing quite a bit of help.@brThanks to @a1https://marina-magnolia.tumblr.com/@a2Marina Magnolia Venegas@a3 for doing testing and QA.")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 20,
+		decoration: {width:90,height:380},
+		ui: {width:322,height:408,deg:40}
+	},{
+		title: "MPaint",
+		id: "mp",
+		color: "teal",
+		rating: "(Good)",
+		tagline: "A sampler from a popular video game.",
+		description: `
+forget about warmth.<br/>
+this distortion gives off a harsh and metallic sound with an FM vibe that is guaranteed to destroy any mix.<br/>
+(sounds real phat and juicy on bass)<br/>
+<a class="demo" href="javascript:void(0);" onclick='popup_audio(10, "/pf/demo1.mp3", "Demo made by @a1https://soundcloud.com/the_real_astrodex@a2Astrodex@a3.")'>Demo 1</a>
+<a class="demo" href="javascript:void(0);" onclick='popup(11, "Plastic Funeral credits:@brMade by Ari Hanan (@a1https://unplug.red/@a2unplugred@a3)@brThanks to @a1https://laulaulau.bandcamp.com/@a2lau@a3 for creating the factory presets as well as providing quite a bit of help.@brThanks to @a1https://marina-magnolia.tumblr.com/@a2Marina Magnolia Venegas@a3 for doing testing and QA.")'>Credits</a>
+<br/>
+<br/>
+Cost: +0$<br/>
+Available for both windows and linux`,
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		decoration: {width:90,height:380},
+		ui: {width:468,height:40,deg:40}
+	},{
+		title: "CRMBL",
+		id: "crmbl",
+		color: "teal",
+		rating: "(Very good)",
+		tagline: "Delay for insane people.",
+		description: "",
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 25,
+		decoration: {width:90,height:380},
+		ui: {width:507,height:465,deg:30}
+	},{
+		title: "Prisma",
+		id: "prsm",
+		color: "teal",
+		rating: "(Very good)",
+		tagline: "Modular multiband distortion plugin.",
+		description: "",
+		freedownload: "https://shop.unplug.red/b/wGlzc",
+		paiddownload: "https://shop.unplug.red/b/wGlzc",
+		price: 30,
+		decoration: {width:90,height:380},
+		ui: {width:478,height:561,deg:20}
+	}
+]
